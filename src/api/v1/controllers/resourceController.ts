00a6d604e8d9a402a801b44f51e77dfede04c1ef
IvanBanal <ibanal@rrc.ca>
2026-03-19 15:52:18 -0500

    Adding getResourceById function to resourceController.ts.

--- src/api/v1/controllers/resourceController.ts
+++ src/api/v1/controllers/resourceController.ts
@@ -2,7 +2,7 @@ import { Request, Response} from "express";
 import * as service from "../services/resourceService";
 import { HTTP_STATUS } from "../../../constants/httpConstants";
 
-export const getAllResources = (req: Request, res: Resposne) => {
+export const getAllResources = (req: Request, res: Response) => {
     const resources = service.getAllResources();
     res.status(HTTP_STATUS.OK).json({
         message: "Resources retrieved",
@@ -10,4 +10,16 @@ export const getAllResources = (req: Request, res: Resposne) => {
         data: resources
     });
 };
+
+export const getResourceById = (req: Request, res: Response) => {
+    const id = parseInt(req.params.id);
+    const resource = service.getResourceById(id);
+    
+    if (!resource) {
+        res.status(HTTP_STATUS.NOT_FOUND).json({ message: "Resource not found" });
+        return;
+    }
+    res.status(HTTP_STATUS.OK).json({ message: "Resource retrieved", data: resource });
+};
+
 
