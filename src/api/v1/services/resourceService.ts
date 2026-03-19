b0cbc0fac9d38e2b0007b99607d09155d300e0c0
IvanBanal <ibanal@rrc.ca>
2026-03-19 14:42:38 -0500

    Adding getResourceById to resourceService.ts.

--- src/api/v1/services/resourceService.ts
+++ src/api/v1/services/resourceService.ts
@@ -5,3 +5,7 @@ export const getAllResources = () => {
     return resources;
 };
 
+export const getResourceById = (id: number) => {
+    return resources.find(resources => resources.id === id);
+};
+
