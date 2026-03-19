de5cc4dcb5610780f57fd494b5720a1120a78454
IvanBanal <ibanal@rrc.ca>
2026-03-19 14:46:57 -0500

    Adding createResource function to resourceService.ts.

--- src/api/v1/services/resourceService.ts
+++ src/api/v1/services/resourceService.ts
@@ -9,3 +9,16 @@ export const getResourceById = (id: number) => {
     return resources.find(resources => resources.id === id);
 };
 
+export const createResource = (data: Partial<Resource>) => {
+    const newResource: Resource = {
+        id: resources.length + 1,
+        title: data.title!,
+        type: data.type!,
+        url: data.url!,
+        description: data.description || "",
+        createdAt: new Date().toISOString()
+    };
+    
+    resources.push(newResource);
+    return newResource;
+};
